d24a3183786996b2c983ed04211fc08741dede57
syed mustafa ali rizvi <mustafariz15@gmail.com>
2022-11-22 12:06:12 -0800

    fix session route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults {format :json } do
     resources :users, only: :create
-    resources :session, only: [:show, :create, :destroy]
+    resource :session, only: [:show, :create, :destroy]
   end
 
 
