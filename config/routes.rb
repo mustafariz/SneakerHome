98fca5f6f779b51827cf89661da7582d8fdc14e2
syed mustafa ali rizvi <mustafariz15@gmail.com>
2022-11-22 12:04:26 -0800

    Create  authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults {format :json } do
+    resources :users, only: :create
+    resources :session, only: [:show, :create, :destroy]
+  end
+
+
 end
